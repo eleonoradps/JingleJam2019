9fc3f48820a278713e2b5f412c562224a7bd2c99
L <eleonora.dups@gmail.com>
2019-12-16 16:30:04 +0100

    added gift counter

--- Assets/Scripts/PickUpGifts.cs
+++ Assets/Scripts/PickUpGifts.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PickUpGifts : MonoBehaviour
 {
-    [SerializeField] private int giftValue = 1;
+    [SerializeField] private int value = 1;
     private SpriteRenderer spriteRenderer;
     void Start()
     {
@@ -21,7 +21,9 @@ public class PickUpGifts : MonoBehaviour
         if(collider.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
             Destroy(gameObject);
+            collider.GetComponent<TestPlayerController>().AddGift(value);
         }
+
     }
 
     
--- Assets/Scripts/TestPlayerController.cs
+++ Assets/Scripts/TestPlayerController.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TestPlayerController : MonoBehaviour
 {
     private Rigidbody2D body;
     private Vector2 direction;
     private float speed = 3f;
+
+    private int gift;
+    [SerializeField] private TextMeshProUGUI textGiftCounter;
     
     void Start()
     {
@@ -21,5 +26,11 @@ public class TestPlayerController : MonoBehaviour
     void Update()
     {
         direction = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+    }
+
+    public void AddGift(int value)
+    {
+        gift += value;
+        textGiftCounter.text = gift.ToString();
     }
 }

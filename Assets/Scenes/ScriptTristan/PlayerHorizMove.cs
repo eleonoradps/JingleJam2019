fbcc620aabcc4955328d29e0ea2f72e771138657
SaturnCitizen <tristan.schorr@hotmail.fr>
2019-12-16 17:27:58 +0100

    adding sprite color change and speed change based on size value

--- Assets/Scenes/ScriptTristan/PlayerHorizMove.cs
+++ Assets/Scenes/ScriptTristan/PlayerHorizMove.cs
@@ -4,21 +4,55 @@ using UnityEngine;
 
 public class PlayerHorizMove : MonoBehaviour
 {
-    [SerializeField] private int maxSpeed = 15;
+    [SerializeField] private int maxSpeed = 5;
 
-    private Vector2 Velocity;
+    [SerializeField] private int sizeThreshold = 0;
 
     private Rigidbody2D rb2d;
+
+    private SpriteRenderer sprite;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+
+        increaseTest();
+
+        if (sizeThreshold <= 10)
+        {
+            sprite.color = new Color(255, 0, 0, 255);
+            maxSpeed = 15;
+        }
+        else if (sizeThreshold >= 11 && sizeThreshold <= 20)
+        {
+            sprite.color = new Color(0, 255, 0, 255);
+            maxSpeed = 10;
+        }
+        else if (sizeThreshold >= 21)
+        {
+            sprite.color = new Color(0, 0, 255, 255);
+            maxSpeed = 5;
+        }
     }
 
     void FixedUpdate()
     {
-        float x = Input.GetAxis("Horizontal");
+        float x = Input.GetAxisRaw("Horizontal");
         Vector3 move = new Vector3(x * maxSpeed, rb2d.velocity.y, 0f);
         rb2d.velocity = move;
     }
+
+    void increaseTest()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            sizeThreshold += 1;
+        }
+    }
+
 }

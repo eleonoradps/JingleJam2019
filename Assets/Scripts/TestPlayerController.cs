b688dd707ee54ef67305e1530f7a5895d383f6d6
L <eleonora.dups@gmail.com>
2019-12-16 15:55:22 +0100

    pick up gifts script done & working

---
+++ Assets/Scripts/TestPlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerController : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private Vector2 direction;
+    private float speed = 3f;
+    
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        body.velocity = new Vector2(direction.x * speed, body.velocity.y);
+    }
+
+    void Update()
+    {
+        direction = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+    }
+}

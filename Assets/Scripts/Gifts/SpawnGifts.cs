81c3f1a051ded48bcc99c9be536773dd4008e513
Solange <sol.sch1990@gmail.com>
2019-12-17 11:53:24 +0100

    Add autospwan gifts

---
+++ Assets/Scripts/Gifts/SpawnGifts.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGifts : MonoBehaviour
+{
+    [SerializeField] GameObject[] prefabs;
+    private bool gift = true;
+    private float posXMin = -9f;
+    private float posXMax = 9f;
+    private int posY = 7;
+    private int posZ = 0;
+
+
+    void Start()
+    {
+        gift = true;
+        StartCoroutine(coroutineExample());
+    }
+
+    IEnumerator coroutineExample()
+    {
+        while (gift)
+        {
+            int prefab_num = Random.Range(0, prefabs.Length);
+            float posX = Random.Range(posXMin, posXMax);
+            Instantiate(prefabs[prefab_num], new Vector3(posX, posY, posZ), transform.rotation);
+            yield return new WaitForSeconds(2.0f);
+        }
+    }
+}

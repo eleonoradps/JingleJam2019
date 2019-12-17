9242ac3fdcfeadf03e26767c322a7081f15474d9
SaturnCitizen <tristan.schorr@hotmail.fr>
2019-12-17 23:23:41 +0100

    added a lot in gamescene

--- Assets/Scripts/Snowballs/Snowball.cs
+++ Assets/Scripts/Snowballs/Snowball.cs
@@ -8,6 +8,7 @@ public class Snowball : MonoBehaviour
     [SerializeField] float speedY = 5f;
     Rigidbody2D body;
     float posY = -4.5f;
+    private int snowVal = 1;
 
     private void Start()
     {
@@ -18,6 +19,15 @@ public class Snowball : MonoBehaviour
     {
         if (this.transform.position.y <= posY)
         {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            collider.GetComponent<PlayerHorizMove>().GrowSize(snowVal);
             Destroy(gameObject);
         }
     }

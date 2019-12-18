7e8b07223f3482e8a774726418165db6e8215b95
GJeannin0 <j6.guillaume@gmail.com>
2019-12-18 08:14:25 +0100

    added shooting cooldown

---
+++ Assets/Scripts/ScriptGui/SnowShooter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowShooter : MonoBehaviour
+{
+	[SerializeField] private GameObject snowball;
+
+	[SerializeField] private float snowballSpeed;
+
+
+	[SerializeField] private float shootCooldown;
+	private float cooldownTimer = 0.0f;
+	private bool shootReady = true;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+		if (cooldownTimer <= shootCooldown && !shootReady)
+		{
+			cooldownTimer += Time.deltaTime;
+		}
+		else
+		{
+			shootReady = true;
+		}
+
+		if (Input.GetAxis("Fire1") > 0.1f && shootReady)
+		{
+			shootReady = false;
+			cooldownTimer = 0;
+			GameObject shotSnowball = Instantiate(snowball, transform.position, Quaternion.identity);
+			shotSnowball.transform.parent = null;
+			shotSnowball.transform.localScale = Vector3.one;
+
+			shotSnowball.GetComponent<Rigidbody2D>().velocity = Vector2.up * snowballSpeed;
+			Destroy(shotSnowball, 2);
+		}
+	}
+
+
+}
